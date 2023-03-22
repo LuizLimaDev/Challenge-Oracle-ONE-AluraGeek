e90774ddd722404606e88e745a8ae82dfa26e411
luizlimadev <devluizlima@gmail.com>
2023-03-22 20:01:41 -0300

    Pagina produtos v1.0

--- src/js/card-build.js
+++ src/js/card-build.js
@@ -1,10 +1,11 @@
 //template de cada card de produto
-export function cardBuild(imageUrl, alt, name, price) {
+export function cardBuild(imageUrl, alt, name, price, id) {
   const product = document.createElement('div');
   product.className = 'product-box';
   product.innerHTML = `<img src="${imageUrl}" alt="${alt}" class="product-box-img">
   <h4 class="product-box-title">${name}</h4>
   <p class="product-box-price">${price}</p>
+  <p class="product-id hide">#${id}</p>
   <a href="#" class="product-box-link">Ver produto</a>
   `
 
--- src/js/show-all-products.js
+++ src/js/show-all-products.js
@@ -2,13 +2,10 @@ import { produtosService } from './products-service.js'
 import { cardBuild } from './card-build.js'
 
 //selecionando categorias de produtos
-const starWarsList = document.querySelector('.starWars');
-const consolesList = document.querySelector('.consoles');
-const miscList = document.querySelector('.diversos');
 const allProducts = document.querySelector('.container-allProductsShow');
 
 // Criando as sessoes dinamicamente
-function cardBuilder(list, categoria) {
+function cardBuilder(categoria) {
   let categoryList = ['starWars', 'consoles', 'diversos'];
   let categoryGroup = categoryList.map(valores => produtosService.productsList(valores))
   Promise.all(categoryGroup)
@@ -16,12 +13,12 @@ function cardBuilder(list, categoria) {
   produtosService.productsList(categoria)
     .then(data => {
       data.forEach(elemento => {
-        list.appendChild(cardBuild(elemento.imageUrl, elemento.alt, elemento.name, elemento.price, elemento.id, elemento.section))
+        allProducts.appendChild(cardBuild(elemento.imageUrl, elemento.alt, elemento.name, elemento.price))
       })
     })
 }
 
-cardBuilder(starWarsList, 'starWars')
-cardBuilder(consolesList, 'consoles')
-cardBuilder(miscList, 'diversos')
+cardBuilder('starWars')
+cardBuilder('consoles')
+cardBuilder('diversos')
 
